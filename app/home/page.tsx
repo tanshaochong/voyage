0dd0baa880c672f7a94ecb5a96ffbdfa84c9655b
Zhou Ziheng <zhou.ziheng@outlook.com>
2023-10-01 00:03:10 +0800

    feat: change styling

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -2,7 +2,6 @@ import { ArrowRight, MessageSquareIcon } from 'lucide-react';
 import Link from 'next/link';
 
 import { FeedbackForm, Goals, ProfileInfo, Progress } from '@/components/home/';
-import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert';
 import {
   AlertDialog,
   AlertDialogContent,
@@ -30,61 +29,71 @@ const extractInitials = (fullName: String): String => {
 
 export default function HomePage() {
   return (
-    <div className="container grid grid-cols-2 justify-center w-full h-full box-border gap-4 py-4">
-      <div className="w-full flex flex-col grow">
-        <Card className="p-6 h-full w-full flex flex-col gap-2 items-center">
-          <div>
-            <Avatar className="w-24 h-24 mb-4">
-              <AvatarImage />
-              <AvatarFallback>
-                {extractInitials(FIREBASE_USER.displayName)}
-              </AvatarFallback>
-            </Avatar>
-            <div className="text-2xl font-semibold">
-              {FIREBASE_USER.displayName}
+    <div className="container py-4">
+      <h1 className="text-3xl font-semibold text-left mr-auto mb-8">
+        Hi {FIREBASE_USER.displayName.split(' ')[0]},
+        <br />
+        Welcome to your voyage
+      </h1>
+      <div className="grid grid-cols-2 justify-center w-full h-full box-border gap-4">
+        <div className="w-full flex flex-col grow">
+          <Card className="p-6 h-full w-full flex flex-col gap-2 items-center">
+            <div>
+              <Avatar className="w-24 h-24 mb-4">
+                <AvatarImage />
+                <AvatarFallback>
+                  {extractInitials(FIREBASE_USER.displayName)}
+                </AvatarFallback>
+              </Avatar>
+              <div className="text-2xl font-semibold">
+                {FIREBASE_USER.displayName}
+              </div>
+              <small className="text-sm text-muted-foreground leading-none">
+                {USER.positions[USER.positions.length - 1].title}
+              </small>
             </div>
-            <small className="text-sm text-muted-foreground leading-none">
-              {USER.positions[USER.positions.length - 1].title}
-            </small>
-          </div>
-          <ProfileInfo skills={USER.skills} />
-          {/* <Alert>
+            <ProfileInfo skills={USER.skills} />
+            {/* <Alert>
             <AlertTitle>New courses available!</AlertTitle>
             <AlertDescription>
               Check out new courses to boost your performance.
-            </AlertDescription>
+              </AlertDescription>
           </Alert> */}
-          <Goals />
-        </Card>
-      </div>
-      <div className="w-full flex flex-col grow">
-        <Card className="w-full flex flex-col grow mb-5">
-          <CardHeader>
-            <CardTitle>Track your progress</CardTitle>
-            <CardDescription>Your PSA journey, at a glance</CardDescription>
-          </CardHeader>
-          <CardContent>
-            <Progress skills={USER.skills} />
-          </CardContent>
-          <CardFooter className="mt-auto">
-            <Link
-              href="/overview"
-              className={cn(buttonVariants({ variant: 'outline' }), 'ml-auto')}
-            >
-              <ArrowRight className="h-5 mr-3" />
-              Find out more
-            </Link>
-          </CardFooter>
-        </Card>
-        <Card className="w-full">
-          <CardHeader>
-            <CardTitle>Give feedback, to anyone</CardTitle>
-            <CardDescription>Give feedback to your peers</CardDescription>
-          </CardHeader>
-          <CardFooter className="justify-end align-bottom h-fit pr-5">
-            <FeedbackFormDialog />
-          </CardFooter>
-        </Card>
+            <Goals />
+          </Card>
+        </div>
+        <div className="w-full flex flex-col grow">
+          <Card className="w-full flex flex-col grow mb-5">
+            <CardHeader>
+              <CardTitle>Track your progress</CardTitle>
+              <CardDescription>Your PSA journey, at a glance</CardDescription>
+            </CardHeader>
+            <CardContent>
+              <Progress skills={USER.skills} />
+            </CardContent>
+            <CardFooter className="mt-auto">
+              <Link
+                href="/overview"
+                className={cn(
+                  buttonVariants({ variant: 'outline' }),
+                  'ml-auto'
+                )}
+              >
+                <ArrowRight className="h-5 mr-3" />
+                Find out more
+              </Link>
+            </CardFooter>
+          </Card>
+          <Card className="w-full">
+            <CardHeader>
+              <CardTitle>Give feedback, to anyone</CardTitle>
+              <CardDescription>Give feedback to your peers</CardDescription>
+            </CardHeader>
+            <CardFooter className="justify-end align-bottom h-fit pr-5">
+              <FeedbackFormDialog />
+            </CardFooter>
+          </Card>
+        </div>
       </div>
     </div>
   );
--- app/technical/page.tsx
+++ app/technical/page.tsx
@@ -1,5 +1,6 @@
 import SkillSection from '@/components/techskill/technical-skill-section';
 import courseData from '@/data/courses.json';
+import { FIREBASE_USER } from '@/data/data';
 
 export type Course = {
   id: string;
@@ -21,9 +22,11 @@ export type Categories = {
 
 export default function TechnicalSkillPage() {
   return (
-    <div className="flex flex-col justify-center">
-      <h1 className="mt-10 text-2xl font-medium text-center mb-10">
-        Empowering Your Career. Curated For you.
+    <div className="container py-4">
+      <h1 className="text-3xl font-semibold text-left mr-auto mb-8">
+        Hi {FIREBASE_USER.displayName.split(' ')[0]},
+        <br />
+        Let&apos;s empower your career with these curated courses
       </h1>
       <div className="flex flex-col items-center">
         {courseData.mainCategory.map((categories, idx) => (
