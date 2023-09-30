7d2a33e872921032f90463693f5fa57b38c51bfe
Ryan Lau <lauquanfengryan@gmail.com>
2023-10-01 03:10:12 +0800

    feat: added course personalisation

--- app/technical/page.tsx
+++ app/technical/page.tsx
@@ -1,6 +1,8 @@
+import PersonaliseSection from '@/components/techskill/personalised-section';
 import SkillSection from '@/components/techskill/technical-skill-section';
 import courseData from '@/data/courses.json';
 import { FIREBASE_USER } from '@/data/data';
+import recommendData from '@/data/recommended.json';
 
 export type Course = {
   id: string;
@@ -23,11 +25,14 @@ export type Categories = {
 export default function TechnicalSkillPage() {
   return (
     <div className="container py-4">
-      <h1 className="text-3xl font-semibold text-left mr-auto mb-4">
+      <h1 className="text-3xl font-semibold text-left mr-auto mb-12">
         Hi {FIREBASE_USER.displayName.split(' ')[0]},
         <br />
         Let&apos;s empower your career with these curated courses
       </h1>
+      <div className="mb-10">
+        <PersonaliseSection recommendedCourses={recommendData.courses} />
+      </div>
       <div className="flex flex-col items-center">
         {courseData.mainCategory.map((categories, idx) => (
           <div className="flex justify-center mb-10" key={idx}>
