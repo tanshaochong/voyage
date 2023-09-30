07622d064f5dae565ff8ea915928aa70ad36d548
Ryan Lau <lauquanfengryan@gmail.com>
2023-09-30 17:10:40 +0800

    feat: fixed course category rendering

--- components/techskill/technical-course-card.tsx
+++ components/techskill/technical-course-card.tsx
@@ -3,41 +3,40 @@ import { useEffect, useState } from 'react';
 import { Course } from '@/app/technical/page';
 
 export default function CourseCard({ course }: { course: Course }) {
-  const [showPlusButton, setShowPlusButton] = useState(false);
-  const [visibleSkills] = useState(course.skills.slice(0, 3));
-  const [remainingSkillsCount] = useState(course.skills.length - 1);
+  // const [showPlusButton] = useState(false);
+  const [visibleSkills] = useState(course.skills);
 
-  useEffect(() => {
-    // Function to check if the buttons occupy more than two lines
-    const checkButtonOverflow = () => {
-      const buttons = document.querySelectorAll('.skills-button');
-      let totalHeight = 0;
-      let lineCount = 0;
+  // useEffect(() => {
+  //   // Function to check if the buttons occupy more than two lines
+  //   const checkButtonOverflow = () => {
+  //     const buttons = document.querySelectorAll('.skills-button');
+  //     let totalHeight = 0;
+  //     let lineCount = 0;
 
-      buttons.forEach((button) => {
-        const buttonHeight = button.clientHeight;
-        totalHeight += buttonHeight;
+  //     buttons.forEach((button) => {
+  //       const buttonHeight = button.clientHeight;
+  //       totalHeight += buttonHeight;
 
-        if (totalHeight > buttonHeight * 2) {
-          lineCount++;
-        }
-      });
+  //       if (totalHeight > buttonHeight * 2) {
+  //         lineCount++;
+  //       }
+  //     });
 
-      setShowPlusButton(lineCount > 0 && remainingSkillsCount > 0);
-    };
+  //     setShowPlusButton(lineCount > 0 && remainingSkillsCount > 0);
+  //   };
 
-    // Call the function when the component mounts and when the window is resized
-    checkButtonOverflow();
-    window.addEventListener('resize', checkButtonOverflow);
+  //   // Call the function when the component mounts and when the window is resized
+  //   checkButtonOverflow();
+  //   window.addEventListener('resize', checkButtonOverflow);
 
-    // Clean up the event listener when the component unmounts
-    return () => {
-      window.removeEventListener('resize', checkButtonOverflow);
-    };
-  });
+  //   // Clean up the event listener when the component unmounts
+  //   return () => {
+  //     window.removeEventListener('resize', checkButtonOverflow);
+  //   };
+  // });
 
   return (
-    <div className="relative flex flex-col rounded-[20px] max-w-[450px] bg-white bg-clip-border shadow-3xl shadow-shadow-500 w-full p-4 border-gray-200 border hover:shadow-lg transition-transform hover:scale-105">
+    <div className="relative flex flex-col rounded-[20px] max-w-[450px] bg-white bg-clip-border shadow-3xl shadow-md shadow-shadow-500 w-full p-4 border-gray-200 border hover:shadow-lg transition-transform hover:scale-105">
       <div className="h-full w-full">
         <div className="mb-3 relative w-full">
           {
@@ -73,11 +72,11 @@ export default function CourseCard({ course }: { course: Course }) {
               {skill}
             </button>
           ))}
-          {showPlusButton && (
+          {/* {showPlusButton && (
             <button className="bg-gray-100 hover:bg-gray-300 duration-300 rounded-xl px-4 py-2 font-light text-sm skills-button">
               ... {remainingSkillsCount}+
             </button>
-          )}
+          )} */}
         </div>
       </div>
     </div>
