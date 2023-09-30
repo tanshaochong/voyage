dbefddfc416a2d67dc9a23314f4e49ec5b1cb165
ackselz <tanshaochong48@gmail.com>
2023-09-30 11:05:37 +0800

    feat: add openai completions route

--- app/interpersonal/page.tsx
+++ app/interpersonal/page.tsx
@@ -1,5 +1,28 @@
+'use client';
+
+import { useCompletion } from 'ai/react';
+
+const DATA = {
+  // good
+};
+
 const InterpersonalPage = () => {
-  return <div>Interpersonal Page</div>;
+  const { completion, input, handleInputChange, handleSubmit } =
+    useCompletion();
+
+  return (
+    <div className="w-full stretch">
+      <form onSubmit={handleSubmit}>
+        <input
+          className="fixed w-full max-w-md bottom-0 border border-gray-300 rounded mb-8 shadow-xl p-2 dark:text-black"
+          value={input}
+          placeholder="Describe your business..."
+          onChange={handleInputChange}
+        />
+      </form>
+      <div className="whitespace-pre-wrap my-6">{completion}</div>
+    </div>
+  );
 };
 
 export default InterpersonalPage;
