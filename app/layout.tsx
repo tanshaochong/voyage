53e46bd9ca1b450d9648e78090b980c5020c3462
Zhou Ziheng <zhou.ziheng@outlook.com>
2023-09-30 13:01:52 +0800

    feat: add logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css';
 
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import Image from 'next/image';
 
 import { SidebarNav } from '@/components/sidebar-nav';
 
@@ -41,9 +42,16 @@ export default function RootLayout({
       <body className={inter.className}>
         <div className="flex flex-col space-y-8 lg:flex-row lg:space-y-0 min-h-screen">
           <aside className="lg:bg-light lg:w-56 px-2 border-r-gray-500 border-r-2">
-            <h1 className="text-4xl text-center py-8 underline text-slate-700 border-b">
+            {/* <h1 className="text-4xl text-center py-8 underline text-slate-700 border-b">
               Voyage
-            </h1>
+            </h1> */}
+            <Image
+              src="/assets/Voyage.png"
+              alt="logo"
+              height={125}
+              width={125}
+              className="mx-auto mt-4"
+            />
             <SidebarNav items={sidebarNavItems} className="py-4" />
           </aside>
           <div className="flex-1 p-6">{children}</div>
